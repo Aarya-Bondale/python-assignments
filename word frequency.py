b0f9c961ea19be873e2f94af411205197e98cee5
Aarya Bondale <67050039+Aarya-Bondale@users.noreply.github.com>
2020-07-26 22:23:01 +0530

    Add files via upload

---
+++ word frequency.py
@@ -0,0 +1,11 @@
+def most_frequent(string):
+    dict = {}
+    for n in string:
+        keys = dict.keys()
+        if n in keys:
+            dict[n] += 1
+        else:
+            dict[n] = 1
+    return dict
+
+print (most_frequent('mississippi'))
